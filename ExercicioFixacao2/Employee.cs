88de40ca0e3eb25e6a34b6d9cc630b98391f39f9
Jailton Monteiro <jailtonmonteiro147@gmail.com>
2024-04-23 17:07:13 -0300

    Adicionar arquivos de projeto.

---
+++ ExercicioFixacao2/Employee.cs
@@ -0,0 +1,36 @@
+﻿using System.Globalization;
+
+namespace ExercicioFixacao2
+{
+    internal class Employee
+    {
+        string name;
+        double glossSalary, tax;
+
+        public double netSalary(){
+            return (glossSalary - tax);
+        }
+        public void increaseSalary(double percentage){
+            double salary;
+
+            salary = (glossSalary + (glossSalary * percentage / 100)) - tax;
+            Console.Write($"Salário Atualizado R$: {salary.ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+
+        public string Name 
+        {
+            get { return name; }
+            set { name = value;}
+        }
+        public double GlossSalary 
+        {
+            get { return glossSalary; }
+            set { glossSalary = value; }
+        }
+        public double Tax 
+        {
+            get { return tax; }
+            set { tax = value; }
+        }
+    }
+}
---
+++ ExercicioFixacao2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+
+namespace ExercicioFixacao2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double percentage;
+
+            Employee employee = new Employee();
+
+            Console.Write("Nome: ");
+            employee.Name = Console.ReadLine();
+            Console.Write("Salário Bruto: ");
+            employee.GlossSalary = double.Parse(Console.ReadLine());
+            Console.Write("Imposto: ");
+            employee.Tax = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Funcionário: {employee.Name}\nSalário R$: {employee.netSalary().ToString("F2", CultureInfo.InvariantCulture)}");
+
+            Console.Write("Percentual de aumento: ");
+            percentage = double.Parse(Console.ReadLine());
+
+            employee.increaseSalary(percentage);
+            
+        }
+    }
+}
